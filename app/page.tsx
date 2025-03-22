f8d6a9e5274a433f515996635a76f46504851808
nabendu82 <nabendu.biswas@gmail.com>
2025-03-22 14:21:12 +0530

    Stage 6 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+      <section>
         Hello WOrld
       </section>
     </>
