53c62efd9139bb34a806fc49858a3b1225ddcf1c
nabendu82 <nabendu.biswas@gmail.com>
2025-03-22 12:14:34 +0530

    Stage 3 done

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "NabenduBlog",
+    url: "https://example.com",
+    description: "HTML, CSS, React, NodeJS and other tech",
+    author: "Nabendu",
+    links: {
+        twitter: "https://twitter.com/nabendu82",
+        github: "https://github.com/nabendu82",
+        personalSite: "https://nabendu.org",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
