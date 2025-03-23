0a5584bab3457f9516b83de888c3b758ff21eb6f
nabendu82 <nabendu.biswas@gmail.com>
2025-03-23 13:08:57 +0530

    CSS basics blogs added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -21,6 +21,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     const currentPage = Number(searchParams?.page) || 1;
     const sortedPosts = sortPosts(posts.filter((post) => post.published));
     const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+    const totalPosts = sortedPosts.length;
 
     const displayPosts = sortedPosts.slice(
         POSTS_PER_PAGE * (currentPage - 1),
@@ -31,7 +32,9 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
         <div className="container max-w-4xl py-6 lg:py-10">
             <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
                 <div className="flex-1 space-y-4">
-                    <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+                    <h1 className="inline-block font-black text-4xl lg:text-5xl">
+                        Blogs <span className="text-muted-foreground text-3xl ml-2">({totalPosts})</span>
+                    </h1>
                     <p className="text-xl text-muted-foreground">
                         My ramblings on all things web dev.
                     </p>
