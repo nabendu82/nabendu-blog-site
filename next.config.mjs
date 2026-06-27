3a5c1f70df75866e5d30551f712783e59e797a9a
nabendu82 <nabendu.biswas@gmail.com>
2026-06-27 12:30:59 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,15 @@
 import { build } from "velite";
 
 /** @type {import('next').NextConfig} */
-export default {
+const nextConfig = {
     // othor next config here...
-    webpack: (config) => {
-        config.plugins.push(new VeliteWebpackPlugin());
-        return config;
-    },
 };
 
-class VeliteWebpackPlugin {
-    static started = false;
-    constructor(/** @type {import('velite').Options} */ options = {}) {
-        this.options = options;
-    }
-    apply(/** @type {import('webpack').Compiler} */ compiler) {
-        // executed three times in nextjs !!!
-        // twice for the server (nodejs / edge runtime) and once for the client
-        compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-            if (VeliteWebpackPlugin.started) return;
-            VeliteWebpackPlugin.started = true;
-            const dev = compiler.options.mode === "development";
-            this.options.watch = this.options.watch ?? dev;
-            this.options.clean = this.options.clean ?? !dev;
-            await build(this.options); // start velite
-        });
-    }
+const isDev = process.env.NODE_ENV === "development";
+
+if (!process.env.VELITE_STARTED) {
+    process.env.VELITE_STARTED = "1";
+    await build({ watch: isDev, clean: !isDev });
 }
+
+export default nextConfig;
